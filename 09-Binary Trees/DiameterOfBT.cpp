5ba7f98b58aa823703ec8ab2e84483f532e231f9
Arth Agarwal <agarwalarth2006@gmail.com>
2026-07-18 21:42:55 +0530

    Solved Binary Tree Problems

---
+++ 09-Binary Trees/DiameterOfBT.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findMax(TreeNode* root, int& diameter){
+        if(!root) return 0;
+        int lh = findMax(root->left,diameter);
+        int rh = findMax(root->right,diameter);
+        diameter = max(diameter,lh+rh);
+        return 1+max(lh,rh);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter;
+        findMax(root,diameter);
+        return diameter;
+    }
+};
