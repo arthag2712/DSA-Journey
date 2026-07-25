0d97bd87725c61c9971993f8578e54ecf6012df5
Arth Agarwal <agarwalarth2006@gmail.com>
2026-07-25 21:08:11 +0530

    Solved Graphs Problem

---
+++ 12-Graphs/CloneGraph.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(node==nullptr) return nullptr;
+        queue<Node*> q;
+        unordered_map<Node*,Node*> mpp;
+        q.push(node);
+        mpp[node] = new Node(node->val);
+        while(!q.empty()){
+            auto curr = q.front();
+            q.pop();
+            for(auto neighbor : curr->neighbors){
+                if(mpp.find(neighbor) == mpp.end()){
+                    mpp[neighbor] = new Node(neighbor->val);
+                    q.push(neighbor);
+                }
+                (mpp[curr]->neighbors).push_back(mpp[neighbor]);
+            }
+        }
+        return mpp[node];
+    }
+};
