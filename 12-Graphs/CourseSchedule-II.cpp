64c42f97e658f374e4c1c950ffb80013ad2246ba
Arth Agarwal <agarwalarth2006@gmail.com>
2026-07-27 00:10:10 +0530

    Solved Graphs Problems

---
+++ 12-Graphs/CourseSchedule-II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prereq) {
+        //use topo sort for this problem
+        vector<vector<int>> adj(numCourses);
+        vector<int> indeg(numCourses,0);
+        for(auto it : prereq){
+            indeg[it[0]]++;
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> topo;
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it : adj[node]){
+                indeg[it]--;
+                if(indeg[it]==0)
+                    q.push(it);
+            }
+        }
+        if(topo.size()<numCourses) return {};
+        return topo;
+    }
+};
